f1fd8b59d708323427150c3fbddb40ae9796197a
longhoang <longhoang1912@gmail.com>
2020-08-25 09:32:39 +0700

    update

--- dev_3.py
+++ dev_3.py
@@ -3,7 +3,8 @@ from api import get_data
 data = get_data()['customers']
 keys = data[0].keys()
 with open("data.csv", 'wt') as file:
-    writer = csv.writer(file, delimiter=',', quotechar='"', quoting=csv.QUOTE_MINIMAL)
+    writer = csv.writer(file, delimiter=',', quotechar='"',
+                        quoting=csv.QUOTE_MINIMAL)
     writer.writerow([key for key in keys])
     for info in data:
-        writer.writerow([ i for i in info.values()])
+        writer.writerow([i for i in info.values()])
