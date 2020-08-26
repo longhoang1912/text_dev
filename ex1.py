e1ab33da1cb2edc4c5103bc0a16d8ec3d10d7f27
longhoang <longhoang1912@gmail.com>
2020-08-26 10:32:28 +0700

    fix

--- ex1.py
+++ ex1.py
@@ -1,17 +1,29 @@
-import random
-nums = '123456789'
-target = 100
-para = ['+', '-', '']
-total = 0
-result = ''
-while True:
-    for index, num in enumerate(nums):
-        if index == len(nums) - 1:
-            result += num
-        else:
-            result += num + random.choice(para)
+data = '123456789'
+index = 1
+count = 0
+list_result = []
 
-    if target == eval(result):
+while True:
+    temp = []
+    for data in list_result:
+        if count + index >= len(data):
+            continue
+        sm = data[:index + count] + "+" + data[index + count:]
+        sub = data[:index + count] + "-" + data[index + count:]
+        non = data[:index + count] + "@" + data[index + count:]
+        temp.append(sm)
+        temp.append(sub)
+        temp.append(non)
+    list_result.extend(temp)
+    temp = [data[:index] + "+" + data[index:],
+            data[:index] + "-" + data[index:],
+            data[:index] + "@" + data[index:]]
+    list_result.extend(temp)
+    index += 1
+    count += 1
+    if index == 9:
         break
-    result = ''
+    
+list_result = set([i.replace("@", "") for i in list_result])
+result = [num for num in list_result if eval(num) == 100]
 print(result)
